11e51a6fa97f2197d288a420819233c1e094a04e
islam-ahmed33 <eslamaborayah@gmail.com>
2023-05-14 08:08:22 -0700

    T1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    list_len = len(my_list) - 1
+    if idx < 0 or idx > list_len:
+        return(None)
+    else:
+        return(my_list[idx])
