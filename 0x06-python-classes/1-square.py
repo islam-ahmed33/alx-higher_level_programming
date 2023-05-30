19502285dd3e95957c5b387fba61dc0a4a6f4126
islam-ahmed33 <eslamaborayah@gmail.com>
2023-05-30 14:15:22 -0700

    T1 update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
+"""Define a Square Class"""
+
 
 class Square:
+    """Represent a Square"""
+
     __size = 0
 
     def __init__(self, size):
+        """
+        Args:
+            self: used by Python to initiate Square instances
+            size (int): size of Square
+        """
         self.__size = size
